38deccce2d75667752cec9149901bf5ca48296c4
nverges <nverges@email.arizona.edu>
2017-07-06 18:04:13 -0700

    framework

---
+++ bamazonCustomer.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+
+    // logs out table - really pretty
+    console.table(res);
+
+  });
+};
+
+
+
